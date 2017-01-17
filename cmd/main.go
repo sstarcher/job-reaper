d697242ea901e1b629b58f091057e58d801b90a5
Justin McCarty <jmccarty@flowbox.local>
2017-01-17 16:09:19 -0500

    Adding support for multiple reaper routines

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"flag"
+	"time"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/sstarcher/job-reaper/config"
 	"github.com/sstarcher/job-reaper/kube"
-	"time"
 )
 
 var (
-	masterURL  = flag.String("master", "", "url to kubernetes api server")
-	configPath = flag.String("config", "./config.yaml", "path to alerter configuration")
-	failures   = flag.Int("failures", -1, "threshold of allowable failures for a job")
-	interval   = flag.Int("interval", 30, "interval in seconds to wait between looking for jobs to reap")
-	logLevel   = flag.String("log", "info", "log level - debug, info, warn, error, fatal, panic")
+	masterURL   = flag.String("master", "", "url to kubernetes api server")
+	configPath  = flag.String("config", "./config.yaml", "path to alerter configuration")
+	failures    = flag.Int("failures", -1, "threshold of allowable failures for a job")
+	interval    = flag.Int("interval", 30, "interval in seconds to wait between looking for jobs to reap")
+	logLevel    = flag.String("log", "info", "log level - debug, info, warn, error, fatal, panic")
+	reaperCount = flag.Int("reapers", 2, "Number of reaper routines to run")
 )
 
 func main() {
@@ -30,7 +32,11 @@ func main() {
 		log.Fatal("No valid alerters")
 	}
 
-	kube := kube.NewKubeClient(*masterURL, *failures, alerters)
+	if *reaperCount < 0 {
+		log.Fatal("reaper count must be greater than 0")
+	}
+
+	kube := kube.NewKubeClient(*masterURL, *failures, alerters, *reaperCount)
 
 	everyTime := time.Duration(*interval) * time.Second
 	for {
